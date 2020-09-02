88a52dd529434a8bb7e5d402c5eced67d0b20312
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-01 22:25:28 -0300

    ponteiros -geral

--- 04_ponteiros.cpp
+++ 04_ponteiros.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <locale.h> // setlocale
-#include <stdlib.h> // system
-#include <string> // Textos em geral
-#include <iomanip>
-
-using namespace std;
-
-
-int main() {
-	setlocale(LC_ALL, "Portuguese");
-	int x;
-	int *px, *py;
-
-	x = 10;
-	px = &x;
-	py = px;//passando p endereço de x p py
-
-	cout << "Endereço de &x: " << &x << endl;
-	cout << "Valor de x: " << x << endl;
-	cout << "Endereço de &px: " << &px << endl;
-	cout << "Conteudo de px:" << px << endl;
-	cout << "Valor de *px: " << *px << endl;
-	cout << *py << endl;
-
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <locale.h> // setlocale
+//#include <stdlib.h> // system
+//#include <string> // Textos em geral
+//#include <iomanip>
+//
+//using namespace std;
+//
+//
+//int main() {
+//	setlocale(LC_ALL, "Portuguese");
+//	int x;
+//	int *px, *py;
+//
+//	x = 10;
+//	px = &x;
+//	py = px;//passando p endereço de x p py
+//
+//	cout << "Endereço de &x: " << &x << endl;
+//	cout << "Valor de x: " << x << endl;
+//	cout << "Endereço de &px: " << &px << endl;
+//	cout << "Conteudo de px:" << px << endl;
+//	cout << "Valor de *px: " << *px << endl;
+//	cout << *py << endl;
+//
+//
+//	system("pause");
+//	return 0;
+//}
