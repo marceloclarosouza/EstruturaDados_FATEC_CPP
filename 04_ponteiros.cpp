b796e7d71875b93632aae5882ca7ef0bfa2aa92e
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-01 22:14:43 -0300

    pointer

--- 04_ponteiros.cpp
+++ 04_ponteiros.cpp
@@ -10,16 +10,19 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 	int x;
-	int *px;
+	int *px, *py;
 
 	x = 10;
 	px = &x;
+	py = px;//passando p endereço de x p py
 
 	cout << "Endereço de &x: " << &x << endl;
 	cout << "Valor de x: " << x << endl;
 	cout << "Endereço de &px: " << &px << endl;
 	cout << "Conteudo de px:" << px << endl;
 	cout << "Valor de *px: " << *px << endl;
+	cout << *py << endl;
+
 
 	system("pause");
 	return 0;
