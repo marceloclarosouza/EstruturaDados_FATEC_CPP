9a3ae9da0c3fab1be05817cefd62cb797752ce2e
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-08 20:08:22 -0300

    falta resolver os exercicios 2 e 3

--- 04_ponteiros.cpp
+++ 04_ponteiros.cpp
@@ -27,3 +27,82 @@
 //	system("pause");
 //	return 0;
 //}
+
+//Exercicio1: Fazer uma função que retorna a soma e a subtração entre dois
+//números.Após a função, imprima os dados na tela.
+//Ex : void calculo(int nro1, int nro2, int* soma, int* sub)
+
+//#include <iostream>
+//#include <locale.h> // setlocale
+//#include <stdlib.h> // system
+//#include <string> // Textos em geral
+//#include <iomanip>
+//
+//using namespace std;
+//
+//void calculo(int num1, int num2, int* soma, int* sub);
+//
+//int main() {
+//	setlocale(LC_ALL, "Portuguese");
+//	int num1, num2, soma, sub;
+//	num1 = 10;
+//	num2 = 5;
+//
+//
+//	calculo(num1, num2, &soma, &sub);
+//	cout << "Soma = " << soma << endl << endl;
+//	cout<<"Sub = " << sub << endl << endl;
+//
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//void calculo(int num1, int num2, int* soma, int *sub) {
+//	*soma = num1 + num2;
+//	*sub = num1 - num2;
+//}
+//
+//exercicio2: ) Fazer uma função para ler e retornar o valor das 4 notas de um aluno
+//e sua média.Após a função, imprima os dados na tela.
+
+//#include <iostream>
+//#include <locale.h> // setlocale
+//#include <stdlib.h> // system
+//#include <string> // Textos em geral
+//#include <iomanip>
+//
+//using namespace std;
+//
+//void calculo(int num1, int num2, int num3, int num4, int* media);
+//
+//int main() {
+//	setlocale(LC_ALL, "Portuguese");
+//	int num1, num2, num3, num4, media;
+//	num1 = 
+//	num2 = 
+//	num3 = 
+//	num4 = 
+//
+//	
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//
+//
+//
+//3) Fazer uma função que recebe um número, calcule e retorne se o
+//número é positivo ou negativo e se é múltiplo ou não de três.Após a
+//função, imprima os dados na tela.
+//Ex: void validarNro(int nro1, bool* positivo, bool* multiploTres)
+
+
+
+
+
+
+
+
+
