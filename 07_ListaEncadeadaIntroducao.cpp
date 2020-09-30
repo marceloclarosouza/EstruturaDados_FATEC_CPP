3d0786fbd26117322f00b15338d14bc5c91da7fe
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-29 21:52:22 -0300

    reutilizando o codigo

--- 07_ListaEncadeadaIntroducao.cpp
+++ 07_ListaEncadeadaIntroducao.cpp
@@ -392,6 +392,211 @@
 //
 //	return 0;
 //}
+//
+//#include <iostream>
+//#include <locale.h>
+//#include <stdlib.h> // system
+//#include <string> // Textos em geral
+//
+//
+//using namespace std;
+//
+//struct Dados {
+//
+//	string nome;
+//	int idade;
+//	float media;
+//	
+//};
+//
+//struct No {
+//
+//	Dados dados;
+//	No* proximo;
+//};
+//
+//struct Lista {
+//
+//	No* inicio;
+//};
+//
+//int main() {
+//
+//	setlocale(LC_ALL, "Portuguese");
+//
+//	Lista* pLista;
+//	pLista = new Lista;
+//
+//	//Criando
+//	No* pMaria;//a struct No aponta para os dados
+//	No* pJose;
+//	No* pJesus;
+//
+//
+//	//criacao dos Nó ou Nodo na memoria
+//	pMaria = new No;//alocando memoria
+//	pMaria->dados.nome = "Maria";
+//	pMaria->dados.idade = 45;
+//	pMaria->dados.media = 9.5;
+//	pMaria->proximo = NULL;
+//
+//	pJose = new No;//alocando memoria
+//	pJose->dados.nome = "Jose";
+//	pJose->dados.idade = 50;
+//	pJose->dados.media = 9;
+//	pJose->proximo = NULL;
+//
+//	pJesus = new No;//alocando memoria
+//	pJesus->dados.nome = "Jesus";
+//	pJesus->dados.idade = 33;
+//	pJesus->dados.media = 10;
+//	pJesus->proximo = NULL;
+//
+//	///Encadeamento do
+//	pLista->inicio = pMaria;
+//	pMaria->proximo = pJose;
+//	pJose->proximo = pJesus;
+//
+//
+//	No* pAtual;
+//
+//	pAtual = pLista->inicio;
+//
+//	while (pAtual != NULL) {
+//		cout << "None: " << pAtual->dados.nome << endl;
+//		cout << "Idade: " << pAtual->dados.idade << endl;
+//		cout << "Media: " << pAtual->dados.media << endl << endl;
+//
+//		pAtual = pAtual->proximo;
+//	}
+//
+//	//libera memoria
+//	delete pMaria;
+//	delete pJose;
+//	delete pJesus;
+//	delete pLista;
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+//
+//#include <iostream>
+//#include <locale.h>
+//#include <stdlib.h> // system
+//#include <string> // Textos em geral
+//
+//
+//using namespace std;
+//
+//struct Dados {
+//
+//	string nome;
+//	int idade;
+//	float media;
+//
+//};
+//
+//struct No {
+//
+//	Dados dados;
+//	No* proximo;
+//};
+//
+//struct Lista {
+//
+//	No* inicio;
+//};
+//
+//int main() {
+//
+//	setlocale(LC_ALL, "Portuguese");
+//
+//	Lista* pLista;
+//	pLista = new Lista;
+//
+//	//Criando
+//	No* pMaria;//a struct No aponta para os dados
+//	No* pJose;
+//	No* pJesus;
+//
+//
+//	//criacao dos Nó ou Nodo na memoria
+//	pMaria = new No;//alocando memoria
+//	pMaria->dados.nome = "Maria";
+//	pMaria->dados.idade = 45;
+//	pMaria->dados.media = 9.5;
+//	pMaria->proximo = NULL;
+//
+//	pJose = new No;//alocando memoria
+//	pJose->dados.nome = "Jose";
+//	pJose->dados.idade = 50;
+//	pJose->dados.media = 9;
+//	pJose->proximo = NULL;
+//
+//	pJesus = new No;//alocando memoria
+//	pJesus->dados.nome = "Jesus";
+//	pJesus->dados.idade = 33;
+//	pJesus->dados.media = 10;
+//	pJesus->proximo = NULL;
+//
+//	///Encadeamento do
+//	pLista->inicio = pMaria;
+//	pMaria->proximo = pJose;
+//	pJose->proximo = pJesus;
+//
+//
+//	No* pAtual;
+//
+//	pAtual = pLista->inicio;
+//
+//	while (pAtual != NULL) {
+//
+//		if (pAtual->dados.nome == "Jose") {
+//			pAtual->dados.media = 7;
+//			break;
+//		}
+//
+//		pAtual = pAtual->proximo;
+//	}
+//
+//	float total = 0;
+//	int qtd = 0;
+//	pAtual = pLista->inicio;
+//
+//	while (pAtual != NULL) {
+//
+//		total += pAtual->dados.media;
+//		pAtual = pAtual->proximo;
+//		qtd++;
+//	}
+//
+//	cout <<"Total: " <<total << endl << endl;
+//	cout << "Media: " << total / qtd << endl << endl;
+//
+//	pAtual = pLista->inicio;
+//
+//	while (pAtual != NULL) {
+//
+//		cout << "None: " << pAtual->dados.nome << endl;
+//		cout << "Idade: " << pAtual->dados.idade << endl;
+//		cout << "Media: " << pAtual->dados.media << endl << endl;
+//
+//		pAtual = pAtual->proximo;
+//	}
+//
+//	//libera memoria
+//	delete pMaria;
+//	delete pJose;
+//	delete pJesus;
+//	delete pLista;
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
 
 #include <iostream>
 #include <locale.h>
@@ -401,72 +606,75 @@
 
 using namespace std;
 
-struct Aluno {
-	string nome;
-	int idade;
-	float media;
+struct Dados {
+	int codigo;
+	string desc;
+	float preco;
 
-	Aluno* proximo;//guarda o endereço do peóximo elemento da lista
+};
+
+struct No {
+	Dados dados;
+	No* proximo;
 };
 
 struct Lista {
-	Aluno* inicio;
+	No* inicio;
 };
 
 int main() {
-
 	setlocale(LC_ALL, "Portuguese");
 
 	Lista* pLista;
 	pLista = new Lista;
 
 	//Criando
-	Aluno* pMaria;
-	Aluno* pJose;
-	Aluno* pJesus;
+	No* pProd1;//a struct No aponta para os dados
+	No* pProd2;
+	No* pProd3;
 
 
 	//criacao dos Nó ou Nodo na memoria
-	pMaria = new Aluno;//alocando memoria
-	pMaria->nome = "Maria";
-	pMaria->idade = 45;
-	pMaria->media = 9.5;
-	pMaria->proximo = NULL;
+	pProd1 = new No;//alocando memoria
+	pProd1->dados.codigo = 100;
+	pProd1->dados.desc = "ovo";
+	pProd1->dados.preco = 5.5;
+	pProd1->proximo = NULL;
 
-	pJose = new Aluno;//alocando memoria
-	pJose->nome = "Jose";
-	pJose->idade = 50;
-	pJose->media = 9;
-	pJose->proximo = NULL;
+	pProd2 = new No;//alocando memoria
+	pProd2->dados.codigo = 200;
+	pProd2->dados.desc = "Coca Cola";
+	pProd2->dados.preco = 8.0;
+	pProd2->proximo = NULL;
 
-	pJesus = new Aluno;//alocando memoria
-	pJesus->nome = "Jesus";
-	pJesus->idade = 33;
-	pJesus->media = 10;
-	pJesus->proximo = NULL;
+	pProd3 = new No;//alocando memoria
+	pProd3->dados.codigo = 300;
+	pProd3->dados.desc = "pao";
+	pProd3->dados.preco = 4.5;
+	pProd3->proximo = NULL;
 
 	///Encadeamento do
-	pLista->inicio = pMaria;
-	pMaria->proximo = pJose;
-	pJose->proximo = pJesus;
+	pLista->inicio = pProd1;
+	pProd1->proximo = pProd2;
+	pProd2->proximo = pProd3;
 
 
-	Aluno* pAtual;
+	No* pAtual;
 
 	pAtual = pLista->inicio;
 
 	while (pAtual != NULL) {
-		cout << "None: " << pAtual->nome << endl;
-		cout << "Idade: " << pAtual->idade << endl;
-		cout << "Media: " << pAtual->media << endl << endl;
+		cout << "Codigo: " << pAtual->dados.codigo << endl;
+		cout << "Desconto: " << pAtual->dados.desc << endl;
+		cout << "Preço: " << pAtual->dados.preco << endl << endl;
 
 		pAtual = pAtual->proximo;
 	}
 
 	//libera memoria
-	delete pMaria;
-	delete pJose;
-	delete pJesus;
+	delete pProd1;
+	delete pProd2;
+	delete pProd3;
 	delete pLista;
 
 	system("pause");
