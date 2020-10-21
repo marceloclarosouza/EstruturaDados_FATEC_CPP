58f6aedd0aa5b2278bde747bee967675efb359c8
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-10-20 22:28:55 -0300

    Exercicio profundidade incompleto

---
+++ Exercicio_profundidade.cpp
@@ -0,0 +1,10 @@
+/*Escreva uma função que conte o número de nós de uma lista
+encadeada.
+Altura.A altura de um nó N em uma lista encadeada é a distância
+entre N e o fim da lista.Mais precisamente, a altura de N é o número
+de passos do caminho que leva de N até o último nó da lista.Escreva
+uma função que calcule a altura de um dado nó.
+Profundidade.A profundidade de um nó N em uma lista encadeada
+é o número de passos do único caminho que vai do primeiro nó da
+lista até N.Escreva uma função que calcule a profundidade de um
+dado nó.*/
