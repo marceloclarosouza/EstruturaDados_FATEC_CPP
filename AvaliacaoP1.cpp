f1494f41fdbbed23d7c9b3ad20ff8bf950ca82b0
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-10-23 12:12:33 -0300

    inicio exercicio 2

--- AvaliacaoP1.cpp
+++ AvaliacaoP1.cpp
@@ -306,3 +306,13 @@ informada na função.*/
 //		ptrNoAtual = ptrNoAtual->proxNo;
 //	}
 //}
+
+
+/*Parte 02
+Crie duas listas chamadas medControlado e medGenerico.Insira, programaticamente, 5
+medicamentos em cada lista.
+Crie uma função que concatene as listas medControlado e medGenerico em uma terceira lista
+chamada medEmEstoque e exiba seus dados.
+Crie uma função que soma todos os preços de compra e venda dos medicamentos da lista
+medEmEstoque e exiba os resultados.*/
+
