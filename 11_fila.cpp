af8b489c79d03f70b726decf3d31492d5de4577c
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-11-17 22:28:58 -0300

    exercicio 2 erra na uniao

--- 11_fila.cpp
+++ 11_fila.cpp
@@ -323,6 +323,365 @@ extra para quem tem média abaixo ou igual a 4.
 Por fim, imprima a fila novamente com os dados alterados.*/
 
 
+//#include <iostream>
+//#include <stdlib.h> 
+//#include <string>
+//
+//using namespace std;
+//
+//
+//// Dados sobre o ALUNO
+//struct Dados {
+//	int matricula;
+//	string nome;
+//	float media;
+//};
+//
+//// Estrutura do Nó
+//struct No {
+//	Dados dados;	// estrutura guardada dentro da lista
+//	No* proxNo;			// aponta para o próximo Nó da lista
+//};
+//
+//// Fila
+//struct Fila {
+//	int qtdNo;
+//	No* inicio;  // cabeça para início
+//	No* final;   // cauda para fim
+//};
+//
+//
+//Fila* criarFila();
+//void liberarFila(Fila* ptrFila);
+//void exibirFila(Fila* ptrFila);
+//void exibirPilhaAvancada(Fila* ptrFila);
+//
+//void mediaMenor4(Fila* ptrFila);
+//
+//bool enfileirarPush(Fila* ptrFila, int matricula, string nome, float media);
+//bool desenfileiraPop(Fila* ptrFila);
+//No* consultarPrimeiroFila(Fila* ptrFila);
+//
+////--------------------------------------------------------
+////						M A I N
+////--------------------------------------------------------
+//int main() {
+//	setlocale(LC_ALL, "Portuguese");
+//
+//	Fila* pFila;
+//
+//	int matricula;
+//	string nome;
+//	float media;
+//
+//	// Cria a FILA
+//	pFila = criarFila();
+//
+//	matricula = 1;
+//	nome = "Creusa";
+//	media = 3.0f;
+//
+//	//--------------------------------------
+//	// Enfileira
+//	//--------------------------------------
+//	enfileirarPush(pFila, matricula, nome, media);
+//	enfileirarPush(pFila, 2, "Lui", 7.0);
+//	enfileirarPush(pFila, 3, "Albert", 2.0);
+//	enfileirarPush(pFila, 4, "Camila", 6.0f);
+//	enfileirarPush(pFila, 5, "Aline", 1.0f);
+//	exibirFila(pFila);
+//
+//	mediaMenor4(pFila);
+//	cout << endl << endl;
+//	exibirFila(pFila);
+//	cout << endl << endl;
+//
+//	////--------------------------------------
+//	//// Desenfileira do INÍCIO da FILA
+//	////--------------------------------------
+//	//desenfileiraPop(pFila);
+//	//desenfileiraPop(pFila);
+//	//desenfileiraPop(pFila);
+//
+//	//cout << "=========================" << endl << endl;
+//	//exibirFila(pFila);
+//
+//
+//	////--------------------------------------
+//	//// Altera o PRIMEIRO elemento da FILA
+//	////--------------------------------------
+//	//No* pNoAluno;
+//
+//	//pNoAluno = consultarPrimeiroFila(pFila);
+//
+//	//if (pNoAluno != NULL) {
+//	//	pNoAluno->dados.nome = "Jesus Maria José";
+//	//	pNoAluno->dados.media = 12.0f;
+//	//}
+//
+//	//cout << "=========================" << endl << endl;
+//	//exibirFila(pFila);
+//
+//
+//	// Libera a memória alocada para a FILA
+//	liberarFila(pFila);
+//
+//
+//	system("pause");
+//	return 0;
+//}
+//
+////--------------------------------------------------------
+//// CRIAR FILA
+////--------------------------------------------------------
+//Fila* criarFila() {
+//	Fila* ptrFila;
+//
+//	ptrFila = new Fila;
+//
+//	// Se a FILA NÃO pode ser criada
+//	if (ptrFila == NULL) {
+//		cout << "Não foi possível criar a fila!" << endl;
+//		return NULL;
+//	}
+//
+//	// Como a FILA está vazia o INÍCIO e o FINAL apontaM para NULL	
+//	ptrFila->qtdNo = 0;
+//	ptrFila->inicio = NULL;
+//	ptrFila->final = NULL;
+//
+//	return ptrFila;
+//}
+//
+////--------------------------------------------------------
+//// LIBERAR FILA
+////--------------------------------------------------------
+//void liberarFila(Fila* ptrFila) {
+//	No* ptrNoAtual;
+//
+//	//Se a FILA NÃO foi criada
+//	if (ptrFila == NULL) {
+//		cout << "A fila não está criada!" << endl;
+//		return;
+//	}
+//
+//	// Exclui cada Nó da FILA
+//	while (ptrFila->inicio != NULL)
+//	{
+//		ptrNoAtual = ptrFila->inicio;
+//
+//		ptrFila->inicio = ptrNoAtual->proxNo;
+//
+//		delete  ptrNoAtual;
+//	}
+//
+//	delete ptrFila;
+//}
+//
+//
+//
+////--------------------------------------------------------
+//// EXIBIR A FILA
+////--------------------------------------------------------
+//void exibirFila(Fila* ptrFila) {
+//	No* ptrNoAtual;
+//
+//	//Se a FILA NÃO foi criada
+//	if (ptrFila == NULL) {
+//		cout << "A pilha não está criada!" << endl;
+//		return;
+//	}
+//
+//	//Se não tiver nenhum Nó na FILA
+//	if (ptrFila->inicio == NULL) {
+//		cout << "A fila esta vazia!" << endl;
+//		return;
+//	}
+//
+//	ptrNoAtual = ptrFila->inicio;
+//
+//	while (ptrNoAtual != NULL) {
+//		cout << "Matrícula: " << ptrNoAtual->dados.matricula << endl;
+//		cout << "Nome: " << ptrNoAtual->dados.nome << endl;
+//		cout << "Média: " << ptrNoAtual->dados.media << endl << endl;
+//
+//		ptrNoAtual = ptrNoAtual->proxNo;
+//	}
+//}
+//
+//
+////--------------------------------------------------------
+//// EXIBIR A FILA
+////--------------------------------------------------------
+//void exibirPilhaAvancada(Fila* ptrFila) {
+//	No* ptrNoAtual;
+//
+//	//Se a FILA NÃO foi criada
+//	if (ptrFila == NULL) {
+//		cout << "A pilha não está criada!" << endl;
+//		return;
+//	}
+//
+//	//Se não tiver nenhum Nó na FILA
+//	if (ptrFila->inicio == NULL) {
+//		cout << "A fila esta vazia!" << endl;
+//		return;
+//	}
+//
+//	ptrNoAtual = ptrFila->inicio;
+//
+//	cout << "===========================================" << endl;
+//	cout << "                   FILA" << endl;
+//	cout << "  Quantidade: " << ptrFila->qtdNo << endl;
+//	cout << "  \tInício: " << ptrFila->inicio << endl;
+//	cout << "===========================================" << endl << endl;
+//
+//
+//	while (ptrNoAtual != NULL) {
+//		cout << "-------------------------------------------" << endl;
+//		cout << "        " << ptrNoAtual << endl;
+//		cout << "Matrícula: " << ptrNoAtual->dados.matricula << endl;
+//		cout << "Nome: " << ptrNoAtual->dados.nome << endl;
+//		cout << "Média: " << ptrNoAtual->dados.media << endl;
+//		cout << "\tPróximo-> " << ptrNoAtual->proxNo << endl;
+//
+//		ptrNoAtual = ptrNoAtual->proxNo;
+//	}
+//	cout << "-------------------------------------------" << endl;
+//	cout << endl;
+//}
+//
+//
+//
+////--------------------------------------------------------
+//// ENFILEIRA
+////--------------------------------------------------------
+//bool enfileirarPush(Fila* ptrFila, int matricula, string nome, float media) {
+//	No* ptrNoNovo;
+//
+//
+//	//Se a FILA NÃO foi criada
+//	if (ptrFila == NULL) {
+//		cout << "A fila não está criada!" << endl;
+//		return false;
+//	}
+//
+//	//---------------------------------------------------------------
+//	//	Cria o novo nó
+//	//---------------------------------------------------------------
+//	ptrNoNovo = new No;
+//
+//	if (ptrNoNovo == NULL) {
+//		cout << "Memória insulficiente!" << endl;
+//		return false;
+//	}
+//
+//	ptrNoNovo->dados.matricula = matricula;
+//	ptrNoNovo->dados.nome = nome;
+//	ptrNoNovo->dados.media = media;
+//	ptrNoNovo->proxNo = NULL;
+//
+//	// Se a fila estiver vazia
+//	if (ptrFila->final == NULL)
+//		ptrFila->inicio = ptrNoNovo;
+//	else
+//		ptrFila->final->proxNo = ptrNoNovo;
+//
+//	ptrFila->final = ptrNoNovo;
+//
+//	// Incrementa o quantidade de Nós
+//	ptrFila->qtdNo++;
+//
+//	return true;
+//}
+//
+//
+//
+////--------------------------------------------------------
+//// DESENFILEIRA
+////--------------------------------------------------------
+//bool desenfileiraPop(Fila* ptrFila) {
+//	No* ptrNoAtual;
+//
+//	// Se a FILA NÃO foi criada
+//	if (ptrFila == NULL) {
+//		cout << "A pilha não está criada!" << endl;
+//		return false;
+//	}
+//
+//	// Cria um ponteiro auxiliar
+//	ptrNoAtual = ptrFila->inicio;
+//
+//	// Ajusta o início da fila
+//	ptrFila->inicio = ptrFila->inicio->proxNo;
+//
+//	// Se for o último nó da fila
+//	if (ptrFila->inicio == NULL) {          // caso tivesse apenas 1 elemento, fica vazia
+//		ptrFila->final = NULL;
+//	}
+//
+//	// Exclui o nó 
+//	delete ptrNoAtual;
+//
+//	// Decrementa o quantidade de Nós
+//	ptrFila->qtdNo--;
+//
+//	return true;
+//}
+//
+//
+////--------------------------------------------------------
+//// CONSULTAR FILA
+////--------------------------------------------------------
+//No* consultarPrimeiroFila(Fila* ptrFila) {
+//	No* ptrNoAtual;
+//
+//	// Se a FILA NÃO foi criada
+//	if (ptrFila == NULL) {
+//		cout << "A fila não está criada!" << endl;
+//		return NULL;
+//	}
+//
+//	ptrNoAtual = ptrFila->inicio;
+//
+//	return ptrNoAtual;
+//}
+//
+//////--------------------------------------
+////// Altera Media < 4
+//////--------------------------------------
+////--------------------------------------------------------
+//void mediaMenor4(Fila* ptrFila) {
+//	No* ptrNoAtual;
+//
+//	// Se a FILA NÃO foi criada
+//	if (ptrFila == NULL) {
+//		cout << "A fila não está criada!" << endl;
+//	}
+//
+//	ptrNoAtual = ptrFila->inicio;
+//
+//	while (ptrNoAtual != NULL) {
+//		if (ptrNoAtual->dados.media <= 4)
+//			ptrNoAtual->dados.media = (ptrNoAtual->dados.media + 1);
+//
+//		ptrNoAtual = ptrNoAtual->proxNo;
+//	}
+//
+//}
+//	
+
+//exercicio2
+/*
+Dadas duas filas com as seguintes matrículas em ordem
+crescente.
+pFilaX ={1, 3, 5, 9,12, 16, 23} e
+pFilaY = {1, 2, 6, 7, 9, 15, 23},
+Implemente a operação UNIÃO, que cria uma terceira fila,
+com a união entre as duas anteriores, também em ordem
+crescente.
+pFilaZ = {1, 2, 3, 5, 6, 7, 9, 12, 15, 16, 23}
+*/
 #include <iostream>
 #include <stdlib.h> 
 #include <string>
@@ -332,9 +691,9 @@ using namespace std;
 
 // Dados sobre o ALUNO
 struct Dados {
-	int matricula;
-	string nome;
-	float media;
+	//int matricula;
+	//string nome;
+	int media;
 };
 
 // Estrutura do Nó
@@ -354,13 +713,13 @@ struct Fila {
 Fila* criarFila();
 void liberarFila(Fila* ptrFila);
 void exibirFila(Fila* ptrFila);
-void exibirPilhaAvancada(Fila* ptrFila);
+//void exibirPilhaAvancada(Fila* ptrFila);
 
-void mediaMenor4(Fila* ptrFila);
+void uniao(Fila* ptrFilaX, Fila* ptrFilaY, Fila* ptrFilaZ);
 
-bool enfileirarPush(Fila* ptrFila, int matricula, string nome, float media);
-bool desenfileiraPop(Fila* ptrFila);
-No* consultarPrimeiroFila(Fila* ptrFila);
+bool enfileirarPush(Fila* ptrFila, int media);
+//bool desenfileiraPop(Fila* ptrFila);
+//No* consultarPrimeiroFila(Fila* ptrFila);
 
 //--------------------------------------------------------
 //						M A I N
@@ -368,63 +727,45 @@ No* consultarPrimeiroFila(Fila* ptrFila);
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 
-	Fila* pFila;
-
-	int matricula;
-	string nome;
-	float media;
+	Fila* pFilaX;
+	Fila* pFilaY;
+	Fila* pFilaZ;	
 
 	// Cria a FILA
-	pFila = criarFila();
+	pFilaX = criarFila();
+	pFilaY = criarFila();
+	pFilaZ = criarFila();
 
-	matricula = 1;
-	nome = "Creusa";
-	media = 3.0f;
-
+	
 	//--------------------------------------
 	// Enfileira
 	//--------------------------------------
-	enfileirarPush(pFila, matricula, nome, media);
-	enfileirarPush(pFila, 2, "Lui", 7.0);
-	enfileirarPush(pFila, 3, "Albert", 2.0);
-	enfileirarPush(pFila, 4, "Camila", 6.0f);
-	enfileirarPush(pFila, 5, "Aline", 1.0f);
-	exibirFila(pFila);
+	enfileirarPush(pFilaX, 1);
+	enfileirarPush(pFilaX, 3);
+	enfileirarPush(pFilaX, 5);
+	enfileirarPush(pFilaX, 9);
+	enfileirarPush(pFilaX, 12);
+	enfileirarPush(pFilaX, 16);
+	enfileirarPush(pFilaX, 23);
+	exibirFila(pFilaX);
+	
+	enfileirarPush(pFilaY, 1);
+	enfileirarPush(pFilaY, 2);
+	enfileirarPush(pFilaY, 6);
+	enfileirarPush(pFilaY, 7);
+	enfileirarPush(pFilaY, 9);
+	enfileirarPush(pFilaY, 15);
+	enfileirarPush(pFilaY, 23);
+	exibirFila(pFilaY);
 
-	mediaMenor4(pFila);
-	cout << endl << endl;
-	exibirFila(pFila);
-	cout << endl << endl;
-
-	////--------------------------------------
-	//// Desenfileira do INÍCIO da FILA
-	////--------------------------------------
-	//desenfileiraPop(pFila);
-	//desenfileiraPop(pFila);
-	//desenfileiraPop(pFila);
-
-	//cout << "=========================" << endl << endl;
-	//exibirFila(pFila);
-
-
-	////--------------------------------------
-	//// Altera o PRIMEIRO elemento da FILA
-	////--------------------------------------
-	//No* pNoAluno;
-
-	//pNoAluno = consultarPrimeiroFila(pFila);
-
-	//if (pNoAluno != NULL) {
-	//	pNoAluno->dados.nome = "Jesus Maria José";
-	//	pNoAluno->dados.media = 12.0f;
-	//}
-
-	//cout << "=========================" << endl << endl;
-	//exibirFila(pFila);
-
+	cout << endl << endl << endl;
+	uniao(pFilaX,pFilaY, pFilaZ);
+	exibirFila(pFilaZ);
 
 	// Libera a memória alocada para a FILA
-	liberarFila(pFila);
+	liberarFila(pFilaX);
+	liberarFila(pFilaY);
+	liberarFila(pFilaZ);
 
 
 	system("pause");
@@ -501,8 +842,8 @@ void exibirFila(Fila* ptrFila) {
 	ptrNoAtual = ptrFila->inicio;
 
 	while (ptrNoAtual != NULL) {
-		cout << "Matrícula: " << ptrNoAtual->dados.matricula << endl;
-		cout << "Nome: " << ptrNoAtual->dados.nome << endl;
+		//cout << "Matrícula: " << ptrNoAtual->dados.matricula << endl;
+		//cout << "Nome: " << ptrNoAtual->dados.nome << endl;
 		cout << "Média: " << ptrNoAtual->dados.media << endl << endl;
 
 		ptrNoAtual = ptrNoAtual->proxNo;
@@ -540,8 +881,8 @@ void exibirPilhaAvancada(Fila* ptrFila) {
 	while (ptrNoAtual != NULL) {
 		cout << "-------------------------------------------" << endl;
 		cout << "        " << ptrNoAtual << endl;
-		cout << "Matrícula: " << ptrNoAtual->dados.matricula << endl;
-		cout << "Nome: " << ptrNoAtual->dados.nome << endl;
+		//cout << "Matrícula: " << ptrNoAtual->dados.matricula << endl;
+		//cout << "Nome: " << ptrNoAtual->dados.nome << endl;
 		cout << "Média: " << ptrNoAtual->dados.media << endl;
 		cout << "\tPróximo-> " << ptrNoAtual->proxNo << endl;
 
@@ -556,7 +897,7 @@ void exibirPilhaAvancada(Fila* ptrFila) {
 //--------------------------------------------------------
 // ENFILEIRA
 //--------------------------------------------------------
-bool enfileirarPush(Fila* ptrFila, int matricula, string nome, float media) {
+bool enfileirarPush(Fila* ptrFila, int media) {
 	No* ptrNoNovo;
 
 
@@ -576,8 +917,6 @@ bool enfileirarPush(Fila* ptrFila, int matricula, string nome, float media) {
 		return false;
 	}
 
-	ptrNoNovo->dados.matricula = matricula;
-	ptrNoNovo->dados.nome = nome;
 	ptrNoNovo->dados.media = media;
 	ptrNoNovo->proxNo = NULL;
 
@@ -651,22 +990,35 @@ No* consultarPrimeiroFila(Fila* ptrFila) {
 //// Altera Media < 4
 ////--------------------------------------
 //--------------------------------------------------------
-void mediaMenor4(Fila* ptrFila) {
-	No* ptrNoAtual;
+void uniao(Fila* ptrFilaX, Fila* ptrFilaY, Fila* ptrFilaZ){
+	No* ptrNoAtualX;
+	No* ptrNoAtualY;
+	No* ptrNoAtualZ;
 
 	// Se a FILA NÃO foi criada
-	if (ptrFila == NULL) {
+	if (ptrFilaZ == NULL) {
 		cout << "A fila não está criada!" << endl;
 	}
 
-	ptrNoAtual = ptrFila->inicio;
+	ptrNoAtualX = ptrFilaX->inicio;
+	ptrNoAtualY = ptrFilaY->inicio;
+	ptrNoAtualZ = ptrFilaY->inicio;
+	
+	while (ptrNoAtualX != NULL){
+		while (ptrNoAtualY != NULL) {
+			if (ptrNoAtualX->dados.media != ptrNoAtualY->dados.media) {
+				enfileirarPush(ptrFilaZ, ptrNoAtualX->dados.media);
+				enfileirarPush(ptrFilaZ, ptrNoAtualY->dados.media);
 
-	while (ptrNoAtual != NULL) {
-		if (ptrNoAtual->dados.media <= 4)
-			ptrNoAtual->dados.media = (ptrNoAtual->dados.media + 1);
+			}
+			else {
+				enfileirarPush(ptrFilaZ, ptrNoAtualX->dados.media);
+			}
 
-		ptrNoAtual = ptrNoAtual->proxNo;
+			ptrNoAtualX = ptrNoAtualX->proxNo;
+			ptrNoAtualY = ptrNoAtualY->proxNo;
+		}
+
 	}
 
 }
-	
